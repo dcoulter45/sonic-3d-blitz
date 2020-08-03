3435ddd4c734515d34e625d42f4b6f3c87368a1a
Dean Coulter <dean@Deans-MacBook-Pro-4.local>
2020-08-03 14:19:16 +0100

    fix ss start pos

--- src/utils/warning.js
+++ src/utils/warning.js
@@ -1,5 +1,5 @@
 var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-console.log(isChrome);
+
 if (!isChrome) {
   var el = document.getElementById("browser-warning");
   el.style.display = "block";
